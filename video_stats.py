eeaf52f2744d0d9d5ffc4066da51138fcbdde67f
MariaLuchnikova <luchnikova80@gmail.com>
2026-03-29 23:46:50 +0300

    upd gitignore

--- video_stats.py
+++ video_stats.py
@@ -1,11 +1,12 @@
 import requests
 import json
+from datetime import date
 
 import os
 from dotenv import load_dotenv
 
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
+# import sys
+# sys.stdout.reconfigure(encoding='utf-8')
 
 load_dotenv(dotenv_path='./.env')
 
@@ -103,8 +104,15 @@ def extract_video_data(vide_ids):
     
     except requests.exceptions.RequestException as e:
         raise e
+    
+def save_to_json(extracted_data):
+    file_path = f'./data/YT_data_{date.today()}.json'
+
+    with open(file_path, 'w', encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    extract_video_data(video_ids)
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
